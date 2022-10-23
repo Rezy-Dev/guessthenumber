62307c1527a854a7ee8693299e681d7896a0f6cc
Raunak Neupane <rezydev@gmail.com>
2022-10-23 13:04:00 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+let randomNumber = Math.floor((Math.random() * 100) + 1)
+console.log("Welcome to Guess Random Number Game!!")
+console.log("The computer will be thinking a Number from 1-100 and you will have to guess it.")
+console.log("___________________________________________________________________________________")
+console.log("Computer Guessed:", randomNumber, "SUSHHHHHHHHHHHHH!!!")
+console.log("___________________________________________________________________________________")
+let userInput;
+let NoOfChances=0;
+let chances; // chances 
+// let userInput = Number.parseInt(prompt("Enter a Number to Guess:"))
+
+do {
+    userInput = Number.parseInt(prompt("Enter a Number to Guess:"))
+    chances = NoOfChances++;
+    if (userInput > 100) {
+        console.log("Enter Value from 1-100 ONLY!!")
+    }
+    else if (userInput < 1) {
+        console.log("Enter Value from 1-100 ONLY")
+    }
+    else if (userInput == randomNumber) {
+        console.log(`You guessed ${userInput} which was CORRECT!!`)
+        let toPrintChance = 100 - chances
+        console.log(`------------SCORE: ${toPrintChance}------------`)
+    }
+} while (userInput != randomNumber)
